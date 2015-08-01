6a4b1dc3c21557bc4e66d0a3d44d357738bf81d9
polartb <chris@dodgersden.com>
2015-07-31 21:28:07 -0400

    Keep Score!

--- pacman/Assets/Scripts/PacDot.cs
+++ pacman/Assets/Scripts/PacDot.cs
@@ -6,6 +6,10 @@ public class PacDot : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.name == "pacman")
+        {
             Destroy(gameObject);
+            ScoreManager.score += 10;
+        }
+            
     }
 }
