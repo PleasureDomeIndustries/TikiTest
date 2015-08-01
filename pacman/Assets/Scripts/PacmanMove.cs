1f958d7d0d4f9c280771831afbe24a06472746bd
whsanders <whsanders@gmail.com>
2015-08-01 02:32:21 -0400

    Fixed independent movement

--- pacman/Assets/Scripts/PacmanMove.cs
+++ pacman/Assets/Scripts/PacmanMove.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class PacmanMove : MonoBehaviour
+public class PacmanMove : NetworkBehaviour
 {
     public float speed = 0.4f;
     Vector2 dest = Vector2.zero;
@@ -13,28 +14,30 @@ public class PacmanMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Move closer to Destination
-        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
-        GetComponent<Rigidbody2D>().MovePosition(p);
-
-        // Check for Input if not moving
-        if ((Vector2)transform.position == dest)
-        {
-            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-                dest = (Vector2)transform.position + Vector2.up;
-            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-                dest = (Vector2)transform.position + Vector2.right;
-            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
-                dest = (Vector2)transform.position - Vector2.up;
-            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-                dest = (Vector2)transform.position - Vector2.right;
-        }
-
-        // Animation Parameters
-        Vector2 dir = dest - (Vector2)transform.position;
-        GetComponent<Animator>().SetFloat("DirX", dir.x);
-        GetComponent<Animator>().SetFloat("DirY", dir.y);
-    }
+		if (isLocalPlayer) {
+			// Move closer to Destination
+			Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+			GetComponent<Rigidbody2D>().MovePosition(p);
+			
+			// Check for Input if not moving
+			if ((Vector2)transform.position == dest)
+			{
+				if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+					dest = (Vector2)transform.position + Vector2.up;
+				if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+					dest = (Vector2)transform.position + Vector2.right;
+				if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+					dest = (Vector2)transform.position - Vector2.up;
+				if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+					dest = (Vector2)transform.position - Vector2.right;
+			}
+			
+			// Animation Parameters
+			Vector2 dir = dest - (Vector2)transform.position;
+			GetComponent<Animator>().SetFloat("DirX", dir.x);
+			GetComponent<Animator>().SetFloat("DirY", dir.y);
+		}
+	}
 
     bool valid(Vector2 dir)
     {
