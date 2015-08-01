40ef4bf2871525906dc869970731ab20f343158a
whsanders <whsanders@gmail.com>
2015-08-01 02:03:36 -0400

    Multiple pacmans!

---
+++ pacman/Assets/PacManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PacManager : NetworkManager {
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) {
+		NetworkServer.AddPlayerForConnection (conn, instantiatePacman (), playerControllerId);
+	}
+
+	GameObject instantiatePacman() {
+		return (GameObject)Instantiate (base.playerPrefab, new Vector3 (14, 14), Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
